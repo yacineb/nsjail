c6dd3c545d38e860118927bfd01be699a28bfd6f
yacineb <yacine@adaptive-ml.com>
2025-06-27 16:48:02 +0200

    ✨ lib64 mount

--- examples/hf_dataset.py
+++ examples/hf_dataset.py
@@ -1,2 +1,4 @@
 from datasets import load_dataset
-print(load_dataset('arbml/Lebanon_Uprising_Arabic_Tweets', split='train')[0])
+
+for i in range(10):
+    print(load_dataset('arbml/Lebanon_Uprising_Arabic_Tweets', split='train')[i])
